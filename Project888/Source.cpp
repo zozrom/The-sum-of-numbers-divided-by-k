b9b6bc5cf8cf879fde0fd59ee55517b49e321b06
zozrom <zozulia.roman135@gmail.com>
2018-12-16 12:29:53 +0200

    Добавьте файлы проекта.

---
+++ Project888/Source.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int sum = 0;
+	int arr[100];
+	int n, k;
+	cout << "Enter the number in array" << endl;
+	cin >> n;
+	cout << "Enter the number k " << endl;
+	cin >> k;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "enter number :" << endl;
+		cin >> arr[i];
+	}
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i]%k==0)
+		{
+			sum += arr[i] / k;
+		}
+	}
+	cout << sum << endl;
+	system("pause");
+}
