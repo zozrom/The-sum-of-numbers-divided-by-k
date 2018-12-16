3a02ae2a53f4d03a0bd30935a6001f2b680e0be7
zozrom <zozulia.roman135@gmail.com>
2018-12-16 12:39:52 +0200

    #Second commit

--- Project888/Source.cpp
+++ Project888/Source.cpp
@@ -12,7 +12,7 @@ int main()
 
 	for (int i = 0; i < n; i++)
 	{
-		cout << "enter number :" << endl;
+		cout << "enter number in array :" << endl;
 		cin >> arr[i];
 	}
 	for (int i = 0; i < n; i++)
